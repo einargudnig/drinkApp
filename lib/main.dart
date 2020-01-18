3999af7f26d8d0d95a97f9a0572e84c6853fbed3
einargudnig <einargudnig@gmail.com>
2020-01-18 21:54:41 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:drink_app/models/user.dart';
+import 'package:drink_app/screens/wrapper.dart';
+import 'package:drink_app/services/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+        child: MaterialApp(
+          home: Wrapper(),
+      ),
+    );
+  }
+}
